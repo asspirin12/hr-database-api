b0bfe61f86bd8c5e9d5f8202cb9e381af4d39ee3
Sergey Kozlovskiy <sergey.kozlovskiy@jetbrains.com>
2022-09-03 22:54:19 +0200

    Add server and handler

--- main.go
+++ main.go
@@ -3,13 +3,30 @@ package main
 import (
 	_ "github.com/lib/pq"
 	"hr-database-api/data"
+	"hr-database-api/handlers"
 	"log"
+	"net/http"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	err := data.ConnectDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	eh := handlers.Employees{}
+	mux := http.NewServeMux()
+
+	mux.Handle("/employees", eh)
+
+	s := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err = s.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
