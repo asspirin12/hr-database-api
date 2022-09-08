3f1b7f053ebb9ef731034bd3d0878b07e6be5ddc
Sergey Kozlovskiy <sergey.kozlovskiy@jetbrains.com>
2022-09-08 13:15:04 +0200

    Add data.GetEmployees and handlers.getEmployees

--- data/employee.go
+++ data/employee.go
@@ -16,6 +16,20 @@ const (
 	dbname   = "guest"
 )
 
+type Employee struct {
+	Id int `json:"id"`
+
+	FirstName string `json:"first_name"`
+
+	LastName string `json:"last_name"`
+
+	Department string `json:"department"`
+
+	Email string `json:"email,omitempty"`
+
+	DateHired string `json:"date_hired"`
+}
+
 // ConnectDatabase checks the connection to the database
 func ConnectDatabase() error {
 	connectionString := fmt.Sprintf("host=%s port=%d user=%s "+
@@ -35,4 +49,41 @@ func ConnectDatabase() error {
 	log.Println("Connected to database")
 	DB = db
 	return nil
+}
+
+// GetEmployees returns a list of employees
+func GetEmployees() ([]Employee, error) {
+	statement := `select id, first_name, last_name, email, department, date_hired from employees limit 10`
+
+	rows, err := DB.Query(statement)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	workforce := make([]Employee, 0)
+
+	for rows.Next() {
+		person := Employee{}
+		err = rows.Scan(
+			&person.Id,
+			&person.FirstName,
+			&person.LastName,
+			&person.Email,
+			&person.Department,
+			&person.DateHired,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		workforce = append(workforce, person)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+	return workforce, nil
 }
--- handlers/employee.go
+++ handlers/employee.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"encoding/json"
+	"hr-database-api/data"
 	"log"
 	"net/http"
 )
@@ -10,7 +12,8 @@ type Employees struct{}
 func (e Employees) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/employees" {
 		if r.Method == http.MethodGet {
-			rw.Write([]byte(("This is hr-database-api")))
+			//rw.Write([]byte(("This is hr-database-api")))
+			e.getEmployees(rw, r)
 		} else {
 			rw.WriteHeader(http.StatusMethodNotAllowed)
 			log.Printf("Method %s is not allowed for %s", r.Method, r.URL.Path)
@@ -20,5 +23,24 @@ func (e Employees) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(http.StatusNotFound)
 		log.Fatalf("Page %s is not found", r.URL.Path)
 		return
+	}
+}
+
+// getEmployees returns a list of employees (limit is 10 records by default)
+func (e Employees) getEmployees(rw http.ResponseWriter, r *http.Request) {
+	log.Println("Handle GET employees")
+
+	employeesList, err := data.GetEmployees()
+	if err != nil {
+		http.Error(rw, "Unable to get employees list", http.StatusInternalServerError)
+		log.Println(err)
+	}
+
+	encoder := json.NewEncoder(rw)
+
+	err = encoder.Encode(employeesList)
+	if err != nil {
+		http.Error(rw, "Unable to encode json", http.StatusInternalServerError)
+		log.Println(err)
 	}
 }
